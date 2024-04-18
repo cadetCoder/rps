b8b1494697e9208a0daf91322cb52b1277420ddc
Your Name <darreltoledo@icloud.com>
2024-04-18 16:58:03 +0100

    Add input and sum functionality in main.py

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 #birth_year = input("Enter your birth year: ")
 #age = 2024 - int(birth_year)
 #print("You are", age)
+
+first = input("First: " )
+second = input("Second: ")
+
+sum = int(first) + int(second)
+
+print(sum)
+
+#print(type(first))
+#print(type(second))
